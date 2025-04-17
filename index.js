243e3aac7b091b27bb6405e55038cdc7d75ea5b5
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-04-17 22:48:36 +0100

    remove repeated code

--- index.js
+++ index.js
@@ -22,43 +22,11 @@ const secondPlayer ={
 }
 
 
-//   //[gameBoard.board[0][0],gameBoard.board[0][1],gameBoard.board[0][2],
-//                   gameBoard.board[1][0],gameBoard.board[1][1],gameBoard.board[1][2],
-//                   gameBoard.board[2][0],gameBoard.board[2][1],gameBoard.board[2][2]]
-
 let positions = gameBoard.board.flat();
-function firstPlayerPlays(){
-    console.log("first player plays X")
-    let random = Math.floor(Math.random()* (positions.length))
-  // return positions.filter( (row)=> row==="null").map((positions)=> positions[random]=firstPlayer.marker)
-for (let choice=0; choice < 1; choice++){
-    let randomPosition = positions[random];
-       if(randomPosition === "null"){
-        positions[random]= firstPlayer.marker ;
-            updateGameBoard()
-       }else{
-        let secondRandom = positions[random];
-        if(secondRandom === "null"){
-            positions[random]= firstPlayer.marker ;
-                updateGameBoard()}
-       }
-    }
-   
 
-};
 
-function secondPlayerPlays(){
-    console.log("Second player plays O")
-    let random = Math.floor(Math.random()* (positions.length))
-    for (let choice=0; choice < 1; choice++){
-       let randomPosition = positions[random];
-       if(randomPosition === "null"){
-        positions[random]= secondPlayer.marker ;
-            updateGameBoard()
-       }else{
-        choice <=1; choice++;
-       }   
-  }}
+firstPlayerPlays(){}
+secondPlayerPlays(){}
 
 
 function playGame(){
@@ -68,9 +36,7 @@ function playGame(){
      if(firstPlayerPlays){
          secondPlayerPlays()
          winningLogic()
-     }
-     
-   
+     }  
 }}playGame()
 
 function updateGameBoard(){
