0ab45369a9a167a6bef96fbfd287de650df164fa
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-04-20 20:32:17 +0100

    update code placements to scope

--- index.js
+++ index.js
@@ -34,40 +34,30 @@ const secondPlayer ={
 
 // play game with our objects.
 function playGame(array){
-    
-    let random = Math.floor(Math.random() * gameState.array.length);
-  if(gameStart === true) {
 
-     //check for empty array
-  for(let arr of array){
-    if (gameState.currentPlayer==="X"&& arr === null){
-           gameState.array[random]= firstPlayer.marker;
-           gameState.switchTurns();
-        }
-           
-        else if(gameState.currentPlayer==="O"&& arr === null){
-            gameState.array[random]= secondPlayer.marker;
-            gameState.switchTurns();
-           }
+if(gameStart === true){ 
 
- }
+            
+            let random = Math.floor(Math.random() * array.length);
+            //check for empty array
+        
+            if (gameState.currentPlayer==="X" && array=== null){
+                gameState.array[random]= firstPlayer.marker; //select random position that is equal to null and put marker.
+                gameState.switchTurns();
+                updateGameBoard()
 
-  }
+                } else if(gameState.currentPlayer==="O"&& array === null){
+                    gameState.array[random]= secondPlayer.marker;//select random position that is equal to null and put marker.
+                    gameState.switchTurns();
+                    updateGameBoard()
+                }
+                    
+                
+       
+       
 
-
-
-
-   const winner= winningLogic(gameState.board);
-   if(winner === "X"){
-    console.log(`${firstPlayer.name} wins`)
-   } else if(winner === "O"){
-    console.log(`${secondPlayer.name} wins`)
-   } else if(winner === "tie"){
-    console.log(`it is a tie.`)
-   }else{
-    console.log("Game in Progress")
-   }
-     }playGame(gameState.board)
+} }
+     playGame(gameState.board)
 
 function updateGameBoard(){
     console.log(gameState.positions)
@@ -75,6 +65,7 @@ function updateGameBoard(){
 }updateGameBoard()
 
 function winningLogic(board){
+    const winner= winningLogic(board);
      let conditions = [[0,1,2],[3,4,5],[6,7,8],   //horizontal matching
                        [0,3,6],[1,4,7],[2,5,8],   // vertical matching
                        [0,4,8],[6,4,2]]           //diagonal matching
@@ -90,6 +81,23 @@ function winningLogic(board){
     }
     return null;
     
+
+
+    function sayWinner(){
+        if(winner === "X"){
+            console.log(`${firstPlayer.name} wins`)
+        } else if(winner === "O"){
+            console.log(`${secondPlayer.name} wins`)
+        } else if(winner === "tie"){
+            console.log(`it is a tie.`)
+        }else{
+            console.log("Game in Progress")
+        }
+        sayWinner();
 }
 
+
+
+}
+winningLogic(gameState.board);
 
